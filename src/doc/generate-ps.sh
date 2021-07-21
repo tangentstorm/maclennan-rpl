a084b8763c6329de6ed2c923843bdc4d060ff236
Bruce J. MacLennan <maclennan@utk.edu>
2021-07-21 06:43:19 -0400

    groff commands to generate postscript.

---
+++ src/doc/generate-ps.sh
@@ -0,0 +1,8 @@
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr FRP >FRP.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr FRP.SNOT >FRP-SNOT.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr RPLIF >RPLIF.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr RPLG >RPLG.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr RPM >RPM.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr RP >RP.ps
+groff -pte -mm -rO1i -Tps eqncharq myeqncharq RPL.hdr RPE.sli >RPE-sli.ps
+
